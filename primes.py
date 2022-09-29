aa743e87912ced137f4fe7ca4a0173bf287b781b
Ben Banks <benjamin.banks960@gmail.com>
2022-09-29 16:46:54 +0100

    Implement value errors

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes>0:
+        i = 0
+        prime = 2
+        while i<number_of_primes:
+            if checkPrime(prime):
+                list.insert(i, prime)
+                prime+=1
+                i+=1
+            else:
+                prime+=1
+    elif number_of_primes<=0:
+        raise ValueError(f"{number_of_primes} is smaller than 1")
+
     return list
+
+def checkPrime(prime):
+    isPrime = True
+    for p in range(2, prime):
+        if (prime%p) == 0:
+            isPrime = False
+    return isPrime
